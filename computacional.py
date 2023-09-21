43b954f6c81cb54d5d6aba5c2c7d9180c7565a0b
SR8Andrew <84993520+SR8Andrew@users.noreply.github.com>
2023-09-21 11:23:46 -0500

    Add files via upload

---
+++ computacional.py
@@ -0,0 +1,13 @@
+egg = int(input("# of calories for egg: "))
+chocolisto = int(input("# of calories for chocolisto: "))
+bread = int(input("# of calories for bread: "))
+pasta = int(input("# of calories for pasta: "))
+watermelon = int(input("# of calories for watermelon: "))
+lettuce = int(input("# of calories for lettuce: "))
+meat = int(input("# of calories for meat: "))
+potatoes = int(input("# of calories for potatoes: "))
+tea = int(input("# of calories for tea: "))
+
+totalcal = egg + chocolisto + bread + pasta + watermelon + lettuce + meat + potatoes + tea
+
+print("The total calories ingested are:", totalcal)
